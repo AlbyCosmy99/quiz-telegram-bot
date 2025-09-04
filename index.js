af8b9d3d649d35602834cba7102324518d13034e
Andrei Albu <albu.cosminandrei.1999@gmail.com>
2025-09-05 00:44:21 +0200

    feat

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ dotenv.config();
 const app = express()
 const port = process.env.PORT ?? 3000
 
+app.get('/', (req,res) => {
+    return res.send('Service is alive.')
+})
+
 app.listen(port, () => {
     bot.launch()
 })
